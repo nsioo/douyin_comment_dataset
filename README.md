e99ddc425253b09285dc0f981d57a95d51268bac
h2222 <1311367609@qq.com>
2020-03-22 00:30:41 +0800

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+### douyin comment dataset
+this is the douyin comment dataset(chinese), each dataset has 8000 ~ 10000 comment data<br>
+dataset_update.xlsx is the preprocossing dataset, all utf-8 format.<br>
---
+++ fix_data.py
@@ -0,0 +1,38 @@
+# coding = utf-8
+
+import pandas as pd
+
+
+
+
+def fix(name):
+    df = pd.read_csv(name+'.csv', encoding='utf-8')
+
+    # print(df.head(50))
+    print(len(df))
+
+    # drop NaN line
+    # df.dropna(axis=0, how='any', inplace=True)
+
+    # select mean columns
+    df = df[['\'序号', '\'评论人', '\'UID', '\'抖音号', '\'获赞数↓', '\'性别↓', '\'子评数↓','\'年龄↓', '\'评论时间↓', '\'评论详情']]
+    # fix header
+    df = df.rename(columns={'\'序号':'id', '\'评论人':'user', '\'UID':'UID', '\'抖音号':'account','\'获赞数↓':'num_like', '\'性别↓':'gender', '\'子评数↓':'sub_comment_num','\'年龄↓':'age', '\'评论时间↓':'comment_time', '\'评论详情':'comment'})
+    # drop the '\''
+    if '2' in name:    
+        df = df.applymap(lambda x: x[1:] if not type(x) is float else x )
+    else:
+        df = df.applymap(lambda x: x[1:])
+    # reset index
+    df = df.reset_index()
+
+    print(df)
+    df.to_excel(name+'_update.xlsx', sheet_name='Sheet1', index=False)
+
+
+
+if __name__ == "__main__":
+    # for name in ['dataset'+str(i) for i in range(2, 7)]:
+        # fix(name) 
+
+    fix('dataset2')
