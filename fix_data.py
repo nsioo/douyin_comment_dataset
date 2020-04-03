10d4f40e670d0bd4c4848fc03bc1708ce125bc75
h2222 <1311367609@qq.com>
2020-04-03 19:50:08 +0800

    update dataset

--- fix_data.py
+++ fix_data.py
@@ -5,21 +5,21 @@ import pandas as pd
 
 
 
-def fix(name):
+def fix(name, sign):
     df = pd.read_csv(name+'.csv', encoding='utf-8')
 
     # print(df.head(50))
     print(len(df))
 
     # drop NaN line
-    # df.dropna(axis=0, how='any', inplace=True)
+    df.dropna(axis=0, how='any', inplace=True)
 
     # select mean columns
     df = df[['\'序号', '\'评论人', '\'UID', '\'抖音号', '\'获赞数↓', '\'性别↓', '\'子评数↓','\'年龄↓', '\'评论时间↓', '\'评论详情']]
     # fix header
     df = df.rename(columns={'\'序号':'id', '\'评论人':'user', '\'UID':'UID', '\'抖音号':'account','\'获赞数↓':'num_like', '\'性别↓':'gender', '\'子评数↓':'sub_comment_num','\'年龄↓':'age', '\'评论时间↓':'comment_time', '\'评论详情':'comment'})
     # drop the '\''
-    if '2' in name:    
+    if sign in name:    
         df = df.applymap(lambda x: x[1:] if not type(x) is float else x )
     else:
         df = df.applymap(lambda x: x[1:])
@@ -35,4 +35,5 @@ if __name__ == "__main__":
     # for name in ['dataset'+str(i) for i in range(2, 7)]:
         # fix(name) 
 
-    fix('dataset2')
+    n = '18'
+    fix('dataset'+n, n)
