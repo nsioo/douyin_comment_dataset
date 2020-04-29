1be8065c01d1893d2bfa17a26b6a32150be67260
h2222 <1311367609@qq.com>
2020-04-29 14:17:23 +0800

    add stop word version

--- word2vec.py
+++ word2vec.py
@@ -14,6 +14,7 @@ import pandas as pd
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 from multiprocessing import Pool
+from functools import partial
 from multiprocessing import cpu_count
 '''
 this script can transfer word to vector , the following step will use:
@@ -36,12 +37,17 @@ def build_emoji_corpus(data):
                     sidx = c.index('[')
                     eidx = c.index(']')
                     c = c[sidx+1:eidx]
-
                     with open(emoji, 'a', newline='\n', encoding='utf_8_sig') as f:
                         f.write(c+'\r\n')
             except:
                 continue
 
+def stop_word(sw_path):
+    with open(sw_path, 'r', encoding='utf_8_sig') as f:
+        swl = []
+        for i in f.readlines():
+            swl.append(i.strip('\n'))
+        return swl
 
 def data_process_xlsx(data):
     jieba.load_userdict('./output/emoji.txt')
@@ -57,13 +63,15 @@ def data_process_xlsx(data):
     return sl
 
 
-def data_process_txt(data):    
+def data_process_txt(data, sw):    
     jieba.load_userdict('./output/emoji.txt')
     with open('./single_vedio/'+data+'.txt', encoding = 'utf_8_sig') as f:
         s1 = []
         for l in f.readlines():
             l = ''.join(re.findall('[\u4e00-\u9fff]', str(l)))
             s = jieba.lcut(l, cut_all=False, HMM=True)
+            # 去停用词
+            s = [i for i in s if not i in sw]
             s1.append(s)
         print('good'+data)
         return (s1, data)
@@ -76,10 +84,14 @@ def add_sentence_corpus(sl, data, save_path='./output/'):
         open(save_path+data+'_s.txt', 'w', encoding='utf_8_sig')
 
     for line in sl:
+        # 空行和单个字行去除
+        if line == []:
+            continue
+        elif len(line) == 1:
+            continue
         with open(save_path+data+'_s.txt', 'a', newline='\n', encoding='utf_8_sig') as f:
             f.write(' '.join(line)+'\r\n')    
     print('good2'+data)
-
 
 
 
@@ -88,7 +100,7 @@ def build_w2v(st_path, save_path):
     program = os.path.basename(sys.argv[0])
     print(program)
 
-    # 建立logger 实例
+     # 建立logger 实例
     logger = logging.getLogger(program)
 
     #1.format: 指定输出的格式和内容，format可以输出很多有用信息，
@@ -115,15 +127,13 @@ def build_w2v(st_path, save_path):
     alpha (float, optional) – 初始学习率
     iter (int, optional) – 迭代次数，默认为5
     '''
-    model = Word2Vec(LineSentence(st_path), size=300, window=2, min_count=5, workers=cpu_count())
+    model = Word2Vec(LineSentence(st_path), size=300, window=3, min_count=5, workers=cpu_count())
     
     # model 保存
     # model.save(save_path)
 
     #不以C语言可以解析的形式存储词向量
     model.wv.save_word2vec_format(save_path, binary=False)
-
-
 
 
 def evaluate(testwords, model='./output/TikTok_word2vec.model'):
@@ -135,25 +145,34 @@ def evaluate(testwords, model='./output/TikTok_word2vec.model'):
 
 
 if __name__ == "__main__":
+
+    swl = stop_word('./output/stopwords.txt')
+
     # build_emoji_corpus('雷军评论')
 
     # 制作 setence corpus
     dsl = ['减肥健康', '情感恋爱', '抗疫', '搞笑2', '搞笑3', '明星李易峰', '罗永浩', '董明珠', '雷军评论']
-    # p = Pool(4)
-    # sls = p.map(data_process_txt, dsl)
-    # p.close()
-    # p.join()
-    # for ls, data in sls:
-    #     # print('data:', data, 'ls', ls)
-    #     # print('--'*20)
-    #     add_sentence_corpus(ls, data)
+    # dsl = ['减肥健康']
+    p = Pool(4)
+    data_process_txt_v2 = partial(data_process_txt, sw=swl)
+    sls = p.map(data_process_txt_v2, dsl)
+    p.close()
+    p.join()
 
+    for ls, data in sls:
+        # print('data:', data, 'ls', ls)
+        print('--'*20)
+        add_sentence_corpus(ls, data)
+    
+    # ================================================== #
     for p, _, fs in [i for i in os.walk('./output')]:
-        for f in fs[1:]:
+        for f in fs[2:]:
             f_p = p+'/'+f
+            print(f_p)
             #制作 word2vector
-            build_w2v(f_p, p+'/'+f[:3]+'.vec')    
+            build_w2v(f_p, p+'/'+'vec/'+f[:3]+'.vec')    
 
+    # ================================================== #
     # test word2vector
     # testwords = ['哈哈', '主播', '可爱', '厉害', '心疼']
     # evaluate(testwords)
